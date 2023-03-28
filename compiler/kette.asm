531ae45e1e03bffe748afefcbe71d39988d93c93
Sebastian Strobl <sebastian@strobl.net>
2023-03-28 02:11:26 +0200

    Add: arguments & TokenStorage

--- compiler/kette.asm
+++ compiler/kette.asm
@@ -5,7 +5,19 @@ include "macros.inc"
 include "linux.inc"
 
 entry $
+  mov rax, [rsp]
+  mov [arg_count], rax
+  mov rax, [rsp + 8]
+  mov [arg_ptr], rax
   call Memory.setup
+
+  getArgCount
+  printlv rax
+
+  mov rcx, 1
+  getArg rcx
+  mov rdi, rax
+  call print_string_nullterm
 
   printlv tkString
   printlv tkNumber
@@ -25,12 +37,6 @@ include "parser.inc"
 
 
 segment readable
-testing db "Hello %s world %d !", 0
-testingLen = $ - testing
-
-testing2 db "to kitten", 0
-testing2Len = $ - testing2
-
 include "constants.inc"
 
 segment readable writable
